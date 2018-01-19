1c47da9247e5f8b6b381d71550151a6de1dbf97c
Youssef Sherif <sharief@aucegypt.edu>
2018-01-19 15:09:14 +0200

    Remove angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CdkTableModule } from '@angular/cdk/table';
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatSortModule,
-  MatTableModule,
-  MatPaginatorModule
-} from '@angular/material';
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { AppComponent } from './app.component';
-import { OverlayModule } from '@angular/cdk/overlay';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { DataService } from './shared/services/data.service';
 import { TableComponent } from './table/table.component';
@@ -53,13 +19,8 @@ import { StateColorMapperService } from './shared/services/state-color-mapper.se
     IconTypeComponent
   ],
   imports: [
-    MatTableModule,
-    MatPaginatorModule,
-    MatRippleModule,
     BrowserAnimationsModule,
     NgProgressModule.forRoot(),
-    CdkTableModule,
-    OverlayModule,
     BrowserModule,
     ChartsModule
   ],
