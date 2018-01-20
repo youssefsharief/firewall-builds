ad4f7989b3869fc4cf2b242be748406e0daf04bc
Youssef Sherif <sharief@aucegypt.edu>
2018-01-20 20:28:12 +0200

    Update test logic and styles

--- src/app/components/charts-box/charts-box.component.ts
+++ src/app/components/charts-box/charts-box.component.ts
@@ -1,22 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-charts-box',
     templateUrl: 'charts-box.component.html',
     styleUrls: ['charts-box.component.scss']
 })
-export class ChartsBoxComponent {
-    @Input() passedPercent: number;
-    @Input() title: string;
-    coveragePercent = 70;
 
-    public themedClass() {
-        if (this.passedPercent < 50) {
-            return 'text-danger';
-        } else if (this.passedPercent < 70) {
-            return 'text-warn';
+export class ChartsBoxComponent implements OnInit {
+    passedPercent: number;
+    themedClass: string;
+    @Input() title: string;
+    @Input() testsData;
+
+
+    ngOnInit() {
+        this.passedPercent = this.testsData.countPassed / this.testsData.countPassed + this.testsData.countFailed;
+        if (this.passedPercent < 0.5) {
+            return this.themedClass = 'text-danger';
+        } else if (this.passedPercent < 0.7) {
+            return this.themedClass = 'text-warn';
         } else {
-            return 'text-success';
+            return this.themedClass = 'text-success';
         }
     }
+
 }
--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,29 +1,69 @@
-import { Component } from '@angular/core';
+import * as Chart from 'chart.js';
+import { ViewChild, Component, ElementRef, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-pie-chart',
     templateUrl: 'pie-chart.component.html',
     styleUrls: ['pie-chart.component.scss']
 })
-export class PieChartComponent {
-    pieChartData = [92, 18];
+export class PieChartComponent implements OnInit {
+    // pieChartData = [92, 18];
 
-    public pieChartColors: Array<any> = [
-        {
-            backgroundColor: [ '#72AC4D', '#EB7D3B'],
-            borderColor: '#72AC4D',
-            pointBackgroundColor: '#72AC4D',
-            pointBorderColor: '#fff',
-        },
+    // public pieChartColors: Array<any> = [
+    //     {
+    //         backgroundColor: [ '#72AC4D', '#EB7D3B'],
+    //         borderColor: '#72AC4D',
+    //         pointBackgroundColor: '#72AC4D',
+    //         pointBorderColor: '#fff',
+    //     },
 
-    ];
+    // ];
 
-    chartHovered(e) {
-        console.log('ssssss');
-    }
+    // chartHovered(e) {
+    //     console.log('ssssss');
+    // }
 
-    chartClicked(e) {
-        console.log('show modal');
+    // chartClicked(e) {
+    //     console.log('show modal');
+    // }
+
+
+    @ViewChild('donut') donut: ElementRef;
+    @Input() countPassed: number;
+    @Input() countFailed: number;
+
+    constructor(
+    ) {   }
+
+    ngOnInit() {
+        const donutCtx = this.donut.nativeElement.getContext('2d');
+
+        const data = {
+            labels: [
+                'Value A',
+                'Value B'
+            ],
+            datasets: [
+                {
+                    'data': [this.countPassed, this.countFailed],
+                    'backgroundColor': [ '#72AC4D', '#EB7D3B']
+                }]
+        };
+
+        const chart = new Chart(
+            donutCtx,
+            {
+                'type': 'pie',
+                'data': data,
+                'options': {
+                    'cutoutPercentage': 50,
+                    'animation': {
+                        'animateScale': true,
+                        'animateRotate': false
+                    }
+                }
+            }
+        );
     }
 
 }
