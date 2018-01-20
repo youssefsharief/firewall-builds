cf7bac2a93811b6ca570209c5e9f015cec010d8a
Youssef Sherif <sharief@aucegypt.edu>
2018-01-20 14:39:53 +0200

    Add percentage input to chart box

--- src/app/components/charts-box/charts-box.component.ts
+++ src/app/components/charts-box/charts-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-charts-box',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
     styleUrls: ['charts-box.component.scss']
 })
 export class ChartsBoxComponent {
+    @Input() percentageMagnitude: number;
 
+
+    public themedClass() {
+        if (this.percentageMagnitude < 50) {
+            return 'text-danger';
+        } else if (this.percentageMagnitude < 70) {
+            return 'text-warn';
+        } else {
+            return 'text-success';
+        }
+    }
 }
