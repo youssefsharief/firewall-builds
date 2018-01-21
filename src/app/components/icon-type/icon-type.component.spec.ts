fb1acfa84d25ed481f73e2b13500d8b49033e37d
Youssef Sherif <sharief@aucegypt.edu>
2018-01-21 11:39:45 +0200

    Refactor "icon-type" component and add spec

--- src/app/components/icon-type/icon-type.component.spec.ts
+++ src/app/components/icon-type/icon-type.component.spec.ts
@@ -1,21 +1,73 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { IconTypeComponent } from './icon-type.component';
+import { StateColorMapperService } from '../../services/state-color-mapper.service';
 
-describe('IconTypeComponent', () => {
+fdescribe('IconTypeComponent', () => {
+
+
+    let fixture: ComponentFixture<IconTypeComponent>;
+    let stateColorMapperService: StateColorMapperService;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 IconTypeComponent
             ],
+            providers: [
+                { provide: StateColorMapperService, useValue: {} }
+            ]
         });
+        fixture = TestBed.createComponent(IconTypeComponent);
+        stateColorMapperService = fixture.debugElement.injector.get(StateColorMapperService);
     }));
 
+
+
     it('should create the component', async(() => {
-        const fixture = TestBed.createComponent(IconTypeComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
 
+
+    it('should render red firewall icon', async(() => {
+        fixture.componentInstance.type = 'firewall';
+        stateColorMapperService.themeColor = () => 'danger';
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-danger.svg"]'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-success.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+    }));
+
+    it('should render green firewall icon', async(() => {
+        fixture.componentInstance.type = 'firewall';
+        stateColorMapperService.themeColor = () => 'success';
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-success.svg"]'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+    }));
+
+
+    it('should render red build icon', async(() => {
+        fixture.componentInstance.type = 'build';
+        stateColorMapperService.themeColor = () => 'danger';
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-success.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+    }));
+
+    it('should render green build icon', async(() => {
+        fixture.componentInstance.type = 'build';
+        stateColorMapperService.themeColor = () => 'success';
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/brickwall-success.svg"]'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('img[src="assets/pc-computer-with-monitor-danger.svg"]'))).toBeFalsy();
+    }));
 
 });
--- src/app/components/icon-type/icon-type.component.ts
+++ src/app/components/icon-type/icon-type.component.ts
@@ -1,20 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StateColorMapperService } from '../../services/state-color-mapper.service';
 
 @Component({
     selector: 'app-icon-type',
     templateUrl: 'icon-type.component.html',
     styleUrls: ['icon-type.component.scss']
-
 })
-export class IconTypeComponent {
+export class IconTypeComponent implements OnInit {
     @Input() type: string;
     @Input() status: string;
+    public themeColor: string;
 
     constructor(
         private stateColorMapperService: StateColorMapperService
     ) {}
-    themeColor() {
-        return this.stateColorMapperService.themeColor(this.status);
+
+    ngOnInit() {
+        this.themeColor = this.stateColorMapperService.themeColor(this.status);
     }
 }
